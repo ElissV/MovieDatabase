9dd9fdd72564897b7d0e006e7e6842fdba9c2b38
8f4md201gv <elis.vassiltsova.v@gmail.com>
2020-04-23 15:20:59 +0300

    Routes setup

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FilmListComponent } from './components/film-list/film-list.component';
+import { FilmComponent } from './components/film/film.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'films', component: FilmListComponent},
+  {path: 'films/:id', component: FilmComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilmListComponent } from './components/film-list/film-list.component';
+import { FilmComponent } from './components/film/film.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilmListComponent
+    FilmListComponent,
+    FilmComponent
   ],
   imports: [
     BrowserModule,
