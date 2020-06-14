3c991d8c465dc5730408a4b03d908cfb4f8ffa56
8f4md201gv <elis.vassiltsova.v@gmail.com>
2020-06-15 01:28:29 +0300

    Review submit form created

--- angular/src/app/components-general/film/film.component.ts
+++ angular/src/app/components-general/film/film.component.ts
@@ -6,6 +6,7 @@ import { GenreService } from 'src/app/services/genre.service';
 import { Review } from 'src/app/classes/review/review';
 import { ReviewService } from 'src/app/services/review.service';
 import { ReviewType } from 'src/app/classes/review-type/review-type';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-film',
@@ -19,17 +20,20 @@ export class FilmComponent implements OnInit {
   reviews: Review[];
   allReviewTypes: ReviewType[];
   dataLoaded: Promise<boolean>;
+  reviewForm: FormGroup;
     
   constructor(private route: ActivatedRoute,
               private filmService: FilmService,
               private genreService: GenreService,
-              private reviewService: ReviewService) { 
+              private reviewService: ReviewService,
+              private formBuilder: FormBuilder) { 
   }
 
   ngOnInit(): void {
     this.getFilmId();
     this.getCurrentFilm();
     this.getReviewTypes();
+    this.initializeForm();
   }
 
   
@@ -69,6 +73,19 @@ export class FilmComponent implements OnInit {
         this.allReviewTypes = data;
       }
     );
+  }
+
+  initializeForm() {
+    this.reviewForm = this.formBuilder.group({
+      type: ['', [Validators.required]],
+      text: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  sendReview() {
+    const typeId = this.reviewForm.controls['type'].value;
+    const text = this.reviewForm.controls['text'].value;
+    this.reviewService.submitReview(typeId, text, this.filmId);
   }
 
 }
--- angular/src/app/services/review.service.ts
+++ angular/src/app/services/review.service.ts
@@ -4,6 +4,8 @@ import { Review } from '../classes/review/review';
 import { HttpClient } from '@angular/common/http';
 import { ReviewType } from '../classes/review-type/review-type';
 import { map } from 'rxjs/operators';
+import { FormGroup } from '@angular/forms';
+import { FilmsWrapper, FilmService } from './film.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +14,15 @@ export class ReviewService {
 
   baseUrl = "http://localhost:8080/api/";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private filmService: FilmService) { }
 
-  getAllReviewTypes(): Observable<ReviewType[]> {
-    const url = this.baseUrl + "reviewTypes";
-    return this.http.get<ReviewTypesWrapper>(url).pipe(
+  
+  getReviewsByFilmId(film: number) {
+    const url = this.baseUrl + "films/" + film + "/filmReviews";
+    return this.http.get<ReviewsWrapper>(url).pipe(
       map(
-        data => data._embedded.reviewTypes
+        data => data._embedded.reviews
       )
     );
   }
@@ -34,13 +38,31 @@ export class ReviewService {
     );
   }
 
-  getReviewsByFilmId(film: number) {
-    const url = this.baseUrl + "films/" + film + "/filmReviews";
-    return this.http.get<ReviewsWrapper>(url).pipe(
+  submitReview(typeId:number, text: string, filmId: number) {
+    const review = this.getReviewObject(typeId, filmId);
+    const url = this.baseUrl + "/reviews";
+    this.http.put<Review>(url, review);
+    console.log("PUT");
+  }
+
+  getReviewObject(typeId: number, filmId: number): Review {
+    const type = this.getCertainType(typeId).subscribe();
+    let review = new Review();
+    review.film = this.filmService.getFilm(filmId).subscribe();
+  }
+  
+  getAllReviewTypes(): Observable<ReviewType[]> {
+    const url = this.baseUrl + "reviewTypes";
+    return this.http.get<ReviewTypesWrapper>(url).pipe(
       map(
-        data => data._embedded.reviews
+        data => data._embedded.reviewTypes
       )
     );
+  }
+
+  getCertainType(id: number): Observable<ReviewType> {
+    const url = this.baseUrl + "reviewTypes" + id;
+    return this.http.get<ReviewType>(url);
   }
 
 }
